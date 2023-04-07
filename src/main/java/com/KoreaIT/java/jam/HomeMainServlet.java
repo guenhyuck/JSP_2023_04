b27f1b1b6d3a2fd8695e6610a18423ed6ffbdef1
guenhycuk <guenhycuk94@gmail.com>
2023-04-07 10:47:59 +0900

    구구단 파라미터 사용

--- src/main/java/com/KoreaIT/java/jam/HomeMainServlet.java
+++ src/main/java/com/KoreaIT/java/jam/HomeMainServlet.java
@@ -7,13 +7,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-@WebServlet("/HomwMainServlet")
+@WebServlet("/home/main")
 public class HomeMainServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+       
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Hello World").append(request.getContextPath());
+		response.getWriter().append("Hello World!").append(request.getContextPath());
 	}
 
 }
