deaff49c3993f88dac616f0e14de0da005b52138
guenhycuk <guenhycuk94@gmail.com>
2023-04-07 10:46:50 +0900

    구구단 기초,파라미터 x

--- src/main/java/com/KoreaIT/java/jam/HomeMainServlet.java
+++ src/main/java/com/KoreaIT/java/jam/HomeMainServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 
 
 @WebServlet("/HomwMainServlet")
-public class HomwMainServlet extends HttpServlet {
+public class HomeMainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
---
+++ src/main/java/com/KoreaIT/java/jam/HomeMainServlet2.java
@@ -0,0 +1,26 @@
+
+package com.KoreaIT.java.jam;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/printDan")
+public class HomeMainServlet2 extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		
+		response.getWriter().append("8단<br>");
+
+		int dan = 8;
+		for (int i = 1; i <= 9; i++) {
+			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));
+		}
+	}
+
+}
